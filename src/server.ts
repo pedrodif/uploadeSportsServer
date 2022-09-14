7bcf6e0720b57739b26cd0e72e713b113498b8a3
Pedro Ferreira <pedrodif@icloud.com>
2022-09-13 21:36:55 -0300

    feat: :art: Prisma ORM install

--- src/server.ts
+++ src/server.ts
@@ -2,27 +2,30 @@ import express from 'express';
 
 const app = express()
 
-// Criando a primeira rota com o método get().
-// O get recebe dois parametros, sendo o primeiro a rota e o segundo parâmetro é uma função,
-// sendo essa a função a ser executada quando o usuário acessar a rota definifa no primeiro parametro -> get()
-// a função que é o segundo parametro do método get, já possui dois parametros predefinidos, sendo eles o request e o response
-// através do parametro response associado ao método .json consigo devolver uma resposta para o usuário em qualquer
-// formato que seja válido podendo ser um array [] ou objeto {}
+app.get('/games', (request, response) => {
+  return response.json([]);
+});
 
-app.get('/ads', (request, response) => {
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+  // const gameId = request.params.id;
   return response.json([
     {id: 1, name: 'Anuncio 1'},
     {id: 2, name: 'Anuncio 2'},
     {id: 3, name: 'Anuncio 3'},
     {id: 4, name: 'Anuncio 4'},
     {id: 5, name: 'Anuncio 5'},
-  ])
+  ]);
 })
 
+app.get('/ads/:id/discord', (request, response) => {
+  // const adId = request.params.id;
+  return response.json([]);
+})
 
-// através do método listen estamos definindo uma porta do localHost
 app.listen(3333)
 
-
-//Através desse código criamos o seguinte endereço:
-// localhost:3333/ads
